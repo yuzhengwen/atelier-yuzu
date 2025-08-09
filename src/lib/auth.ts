8a6ba7d961ba19c3d73d2ee812bff3ce08b7ae94
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-08-09 19:12:04 +0800

    Added debugging text

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,11 @@ import { db } from "./db";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 
+console.log("ENV:", {
+  origin: process.env.VERCEL_URL,
+  DEBUG_TEXT: process.env.DEBUG_TEXT,
+  NODE_ENV: process.env.NODE_ENV
+});
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
